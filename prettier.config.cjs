62516d06ddcdfd28c17991d97ed50de23317146b
Kenneth Castro <kennethramirezcastro@gmail.com>
2024-01-26 11:05:41 -0500

    Initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+// /** @type {import('prettier').Options} */
+// export default {
+//   plugins: [require('prettier-plugin-tailwindcss')],
+// };
+
+
+module.exports = {
+    plugins: [require("prettier-plugin--tailwindcss")]
+}
